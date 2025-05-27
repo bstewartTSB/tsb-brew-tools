51335545574a0fc111ac7943e859fafa2f4f5442
bstewartTSB <brandt.stewart@thirdspacebrewing.com>
2025-05-27 13:05:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cellar-tools-v1';
+const CACHE = 'cellar-tools-v2';
 const FILES = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e =>
