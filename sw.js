e65eb4cb1158a6e42113115e00c4780eebc9efb8
bstewartTSB <brandt.stewart@thirdspacebrewing.com>
2025-05-27 11:58:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
+const CACHE = 'cellar-tools-v1';
+const ASSETS = [
+  '.',          // index.html
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('v1').then(c=>c.addAll(['.','index.html','manifest.json'])));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
+
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(
+      r => r || fetch(e.request)
+    )
+  );
 });
